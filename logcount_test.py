e6e44025130fea79757c0060b4d30c5aa6ab5722
Laoduan <755197244@qq.com>
2026-09-15 22:35:47 +0800

    注释的一些修饰，更方便理解

--- logcount_test.py
+++ logcount_test.py
@@ -30,7 +30,7 @@ def count_log_levels(file_path):
                 elif "DEBUG" in line_upper:
                     cnt["DEBUG"]+=1
 
-            #打印统计结果 
+            # 打印统计结果 
         print("日志级别统计结果:")
         for level,count in cnt.items:
             print(f"{level}: {count}")
@@ -41,15 +41,17 @@ def count_log_levels(file_path):
     except Exception as e:
         print(f"读取文件时发生错误：{e}")
 
-    # 文件自身运行时，__name__为__main__(内置属性)
-    # 当别的文件import本文件时，__name__为文件名(不带后缀)
+    # 当别人导入此文件想复用count_log_levels()时，不会执行下面的代码
+    # 简要来说就是用来判断模块是直接运行还是被导入，避免导入时执行测试代码。
     if __name__ == "__main__":
         # 日志和文件在同一个文件夹下，直接写文件名即可
         # log_file = "app.log"
 
         # 不管在哪运行此文件，都可以计算出脚本所在文件夹，然后去找app.log,都可以找到
-        #先找到脚本的绝对路径，然后去掉文件名字
+        # 先找到脚本所在文件夹的绝对路径，然后去掉文件名
         script_dir = os.path.dirname(os.path.abspath("__file__"))
+        # 然后把脚本所在文件夹和日志名字拼到一起
+        # tips:日志还是要和脚本放在同一目录下的
         log_file = os.path.join(script_dir,"app.log")
 
         count_log_levels(log_file)
